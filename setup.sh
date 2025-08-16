14ce4681042a2e9412013f163eb5dbaea8f0d7b0
imraviagrawal <ravi20036@gmail.com>
2025-08-16 23:52:38 +0000

    setup

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ uv sync --no-install-package flash-attn
 uv sync
 uv run pytest
 
-pip install typing-extensions==4.12.2
-pip install -U torch torchvision --index-url https://download.pytorch.org/whl/cu129
+# pip install typing-extensions==4.12.2
+pip install --force-reinstall torch --index-url https://download.pytorch.org/whl/cu118
 pip install transformers
 pip install datasets
 pip install math-verify[antlr4_13_2]
