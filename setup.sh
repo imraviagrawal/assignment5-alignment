7b8adf46aaa203c8901415b0117673a4838a8aa1
imraviagrawal <ravi20036@gmail.com>
2025-08-17 00:14:18 +0000

    setup removed

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ uv sync --no-install-package flash-attn
 uv sync
 uv run pytest
 
-git config --global user.email "ravi20036@gmail.com"
-git config --global user.name "imraviagrawal"
 # pip install typing-extensions==4.12.2
 # pip install --force-reinstall torch --index-url https://download.pytorch.org/whl/cu118
 # pip install transformers
