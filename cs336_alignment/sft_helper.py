4dae6edbc9c21628e12a37db6c5db493bed4588b
“imraviagrawal” <Ravi20036@gmail.com>
2025-09-18 02:06:08 +0000

    vllm init

--- cs336_alignment/sft_helper.py
+++ cs336_alignment/sft_helper.py
@@ -5,81 +5,76 @@ tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Math-1.5B")
 model = AutoModel.from_pretrained("Qwen/Qwen2.5-Math-1.5B")
 local_directory = "/data/a5-alignment/models/Qwen2.5-Math-1.5B"
 model.save_pretrained(local_directory)
-# tokenizer.save_pretrained(local_directory)
+tokenizer.save_pretrained(local_directory)
 
 
 def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Math-1.5B")):
+    """
+    prompt_strs: list[str]
+    output_strs: list[str]
+    """
     # tokenize input string W/ EOD 
-    # Tokenize Output String W/ EOD     
-    prompt_tokens = tokenizer(prompt_strs)["input_ids"]
-    output_tokens = tokenizer(output_strs)["input_ids"]
+    # Tokenize Output String W/ EOD   
+    # import ipdb; ipdb.set_trace()  
+    # print(type(tokenizer(prompt_strs)))
+    prompt_ids = tokenizer(prompt_strs)['input_ids']
+    output_ids = tokenizer(output_strs)['input_ids']
 
-    batch_sz = len(prompt_tokens)
-    p_o_combined = [len(p) + len(o) for p, o in zip(prompt_tokens, output_tokens)]
-    max_p_o_len = max(p_o_combined)
+    p_o_len = [len(p) + len(o) for p, o in zip(prompt_ids, output_ids)]
+    max_po_len = max(p_o_len)
+    bsz = len(p_o_len)
 
-    input_ids = torch.zeros((batch_sz, max_p_o_len-1))
-    labels = torch.zeros((batch_sz, max_p_o_len-1))
-    response_mask = torch.zeros((batch_sz, max_p_o_len-1), dtype=torch.bool)
+    input_ids = torch.zeros((bsz, max_po_len-1))
+    labels = torch.zeros((bsz, max_po_len-1))
+    response_mask = torch.zeros((bsz, max_po_len-1), dtype=torch.bool)
 
-    for i, (p, o) in enumerate(zip(prompt_tokens, output_tokens)):
-        # iterate on each of the p o pair and construct the batch 
-        p_o_concat = torch.tensor(p + o)
-        concat_len = len(p_o_concat)
-        p_o_concat_padded = F.pad(p_o_concat, (0, max_p_o_len - concat_len), "constant", tokenizer.eos_token_id)
+    for i, (p, o) in enumerate(zip(prompt_ids, output_ids)):
+        p_o_combined = torch.Tensor(p + o)
+        len_p0 = len(p_o_combined)
+        p_o_combined_pad = F.pad(p_o_combined, (0, max_po_len - len_p0), "constant", tokenizer.eos_token_id) # id 
 
-        input_ids[i] = p_o_concat_padded[:-1]
-        labels[i] = p_o_concat_padded[1:]
-
-        o_start = len(p) - 1
-        o_end = concat_len - 1
-        response_mask[i, o_start:o_end] = True
-    return {
-        'input_ids': input_ids,
-        'labels': labels,
-        'response_mask': response_mask,
-    }
+        input_ids[i] = p_o_combined_pad[:-1]
+        labels[i] = p_o_combined_pad[1:]
+        
+        start_o = len(p) - 1
+        end_o = len_p0 - 1
+        response_mask[i, start_o:end_o] = True
 
 
-def compute_entropy(logits: torch.Tensor) -> torch.tensor:
-    # compute softmax 
-    # p_numerator = torch.exp(logits)
-    # p_denom = torch.sum(p_numerator, dim=-1, keepdim=True)
+    return {"input_ids": input_ids, "labels": labels, "response_mask": response_mask}
 
-    # log_prob = logits - torch.logsumexp(logits, dim=-1, keepdim=True)
 
-    # summand = (p_numerator/p_denom)*log_prob
-    # return -torch.sum(summand, dim=-1) # b, s
-    summand = F.softmax(logits, dim=-1)* (logits - torch.logsumexp(logits, dim=-1, keepdim=True))
-    return -torch.sum(summand, dim=-1)
+def compute_entropy(logits: torch.Tensor) -> torch.Tensor:
+    summand = F.softmax(logits, dim=-1) * (logits - torch.logsumexp(logits, dim=-1, keepdim=True))
+    return -1 * torch.sum(summand, dim=-1)
 
-def get_response_log_probs(model, input_ids, labels, return_token_entropy) -> dict[str, torch.Tensor]:
-    logits = model(input_ids).logits
-    log_probs = F.log_softmax(logits, dim=-1)
-    log_probs = torch.gather(log_probs, dim=-1, index=labels.unsqueeze(-1))
-    log_probs = log_probs.squeeze(-1)
-    ret_dict = {}
-    ret_dict['log_probs'] = log_probs
-
-    if return_token_entropy:
-        ret_dict['token_entropy'] = compute_entropy(logits)
+def get_response_log_probs(model, input_ids, labels, return_token_entropy=False):
+    logits = model(input_ids).logits # get logits 
+    log_p = F.log_softmax(logits, dim=-1)
+    token_p = log_p.gather(dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
+    output = {"log_probs": token_p}
     
-    return ret_dict
+    if return_token_entropy:
+        output['token_entropy'] = compute_entropy(logits)
+
+    return output
 
 def masked_normalize(tensor, mask, normalize_constant, dim=None):
-    # if dim == None: dim = -1
-    # masked_tensor = torch.masked_select(tensor, mask)
-    tensor_sum = torch.sum(tensor*mask, dim=dim)
-    tensor_normalize = tensor_sum/normalize_constant
-    return tensor_normalize
+    masked = tensor * mask.to(tensor.dtype)
+    summed = torch.sum(masked, dim=dim)
+    normalized = summed/normalize_constant
+    return normalized
 
-def sft_microbatch_train_step(policy_log_probs, response_mask, gradient_accumulation_steps, normalize_constant):
-    loss = (-masked_normalize(policy_log_probs, response_mask, normalize_constant, -1)).mean()
-    loss /= gradient_accumulation_steps
-    loss.backward()
-    loss_metadata = {
-        'gradient_accumulation_steps': gradient_accumulation_steps,
-        'normalize_constant': normalize_constant
-    }
-
-    return (loss, loss_metadata)
+def sft_microbatch_train_step(policy_log_probs, response_mask, gradient_accumulation_steps, normalize_constant=1.0):
+    loss = -1 * masked_normalize(policy_log_probs, response_mask, normalize_constant, -1).mean()
+    scaled_loss = loss/gradient_accumulation_steps
+    scaled_loss.backward()
+    with torch.no_grad():
+        num_tokens = response_mask.sum().item()
+        stats = {
+            "loss": loss.detach().item(),
+            "num_tokens": num_tokens,
+            "normalize_constant": float(normalize_constant),
+            "gradient_accumulation_steps": gradient_accumulation_steps
+        }
+    return (scaled_loss, stats)
